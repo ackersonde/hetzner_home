c1da0250cc551c75e9f88046687e2ca2edc15230
Dan Ackerson <dan@ackerson.de>
2022-09-24 09:10:32 +0200

    easier

--- scripts/firewall_rules.sh
+++ scripts/firewall_rules.sh
@@ -4,6 +4,7 @@ SERVERS="ubuntu@$MASTER_HOST ubuntu@$SLAVE_HOST"
 for i in $SERVERS
 do
    ssh -o StrictHostKeyChecking=no $i "\
+      ssh-keyscan -t ed25519 $NEW_SERVER_IPV6 | awk '{print $3}' > homepage_host_key && \
       sudo ip6tables -A INPUT -s $NEW_SERVER_IPV6 -m conntrack -p tcp --dport 22 --ctstate NEW,ESTABLISHED -j ACCEPT && \
       sudo ip6tables -D INPUT -s $OLD_SERVER_IPV6 -m conntrack -p tcp --dport 22 --ctstate NEW,ESTABLISHED -j ACCEPT && \
       sudo ip6tables-save -f /etc/iptables/rules.v6"
